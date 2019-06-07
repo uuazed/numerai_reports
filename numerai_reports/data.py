33454aad05a40c6b8b605ccf67d66de75d983895
uuazed <uuazed@gmail.com>
2019-06-07 19:31:44 +0200

    cache results of api queries

--- numerai_reports/data.py
+++ numerai_reports/data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numerapi
 import tqdm
+from joblib import Memory
 
 import utils
 
@@ -31,26 +32,38 @@ query = '''
     }
 '''
 
+napi = numerapi.NumerAPI(verbosity='warn')
+
+memory = Memory("../.cache", verbose=0)
+
+
+@memory.cache
+def fetch_one(round_num, tournament):
+    arguments = {'number': round_num, 'tournament': tournament['tournament']}
+    raw = napi.raw_query(query, arguments)['data']['rounds']
+    if len(raw) > 0:
+        leaderboard = [utils.flatten(d) for d in raw[0]['leaderboard']]
+        df = pd.DataFrame(leaderboard)
+        df['tournament_num'] = tournament['tournament']
+        df['tournament'] = tournament['name']
+        df['round_num'] = round_num
+        df['staking_cutoff'] = str(napi.get_staking_cutoff(
+            round_num, tournament['tournament']))
+        return df
+    return None
+
 
 def fetch_leaderboard(start=0, end=None):
-    napi = numerapi.NumerAPI(verbosity='warn')
 
     dfs = []
     rounds = [start] if end is None else range(start, end + 1)
 
     for round_num in tqdm.tqdm(rounds):
-        for tournament in napi.get_tournaments():
-            tournament_num = tournament['tournament']
-            arguments = {'number': round_num, 'tournament': tournament_num}
-            raw = napi.raw_query(query, arguments)['data']['rounds']
-            leaderboard = [utils.flatten(d) for d in raw[0]['leaderboard']]
-            df = pd.DataFrame(leaderboard)
-            df['tournament_num'] = tournament_num
-            df['tournament'] = tournament['name']
-            df['round_num'] = round_num
-            df['staking_cutoff'] = str(napi.get_staking_cutoff(
-                round_num, tournament_num))
-            dfs.append(df)
+        for tournament in napi.get_tournaments(only_active=False):
+            res = fetch_one(round_num, tournament)
+            if res is not None:
+                dfs.append(res)
+
     df = pd.concat(dfs, sort=False)
     df.columns = [utils.to_snake_case(col) for col in df.columns]
 
