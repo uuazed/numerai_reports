f6c6f686e4d347fb5630799aa06be7e782ba0e87
uuazed <uuazed@gmail.com>
2019-06-15 11:14:27 +0200

    more tests

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -9,20 +9,25 @@ from numerai_reports import data
 def napi(monkeypatch):
 
     def mocked_api(*arg, **kwargs):
-        entry1 = {'username': 'test1',
+        entry1 = {'username': 'looser',
                   'liveAuroc': 0.500,
                   'liveLogloss': 0.6931,
                   'stake': {'value': 1, 'confidence': '0.53'},
                   'stakeResolution': {'destroyed': True},
+                  'paymentStaking': None}
+        entry2 = {'username': 'winner',
+                  'liveAuroc': 0.512,
+                  'liveLogloss': 0.6921,
+                  'stake': {'value': 3, 'confidence': '0.52'},
+                  'stakeResolution': {'destroyed': False},
                   'paymentStaking': {'nmrTransfer': {'value': '0.23'},
                                      'usdAmount': '1.2'}}
-
         return {"data": {"rounds": [
             {'status': 'RESOLVED',
              'benchmark_type': 'auroc',
              'selection': {'bCutoff': '0.501'},
              'leaderboard': [
-                entry1]}
+                entry1, entry2]}
             ]}}
 
     def mocked_tournaments(*args, **kwargs):
@@ -43,12 +48,26 @@ def napi(monkeypatch):
 def test__reputation_bonus(napi):
     res = reports._reputation_bonus(100)
     # staked 1 for 2 tournamences * 50% => 1
-    assert res.loc['test1']['bonus'] == 1
+    assert res.loc['looser']['bonus'] == 1
+    assert res.loc['winner']['bonus'] == 3
 
 
 def test_payments(napi):
     lb = data.fetch_leaderboard(100)
-    res = reports.payments(lb, "test1")
+    res = reports.payments(lb, "looser")
     assert res.loc["total"]['nmr_burned'] == 2
-    assert res.loc["total"]['nmr_total'] == 0.46
+    assert res.loc["total"]['nmr_total'] == 0
+    assert res.loc["total"]['usd_total'] == 0
+    res = reports.payments(lb, "winner")
+    print(res)
+    assert res.loc["total"]['nmr_burned'] == 0
+    assert res.loc["total"]['nmr_total'] == 6.66
     assert res.loc["total"]['usd_total'] == 2.4
+
+
+def test_dominance(napi):
+    lb = data.fetch_leaderboard(100)
+    res = reports.dominance(lb, "winner")
+    assert res.loc[100]['bernie'] == 1
+    res = reports.dominance(lb, "looser")
+    assert res.loc[100]['bernie'] == 0
