12fad3cdac1ca2ed32f4d18d0dc16e495635dd9f
uuazed <uuazed@gmail.com>
2021-04-29 10:33:45 +0200

    flow: split in fetch & upload tasks

--- numerai_reports/flow.py
+++ numerai_reports/flow.py
@@ -15,20 +15,23 @@ import data
 
 @task(log_stdout=False, max_retries=3,
       retry_delay=datetime.timedelta(minutes=10))
-def upload_to_gcs():
+def fetch():
     details, leaderboard = data.fetch_from_api()
+    filename_de = "details.parq"
+    filename_lb = "leaderboard.parq"
+    details.to_parquet(filename_de)
+    leaderboard.to_parquet(filename_lb)
+    return [filename_de, filename_lb]
+
+
+@task(log_stdout=False, max_retries=3,
+      retry_delay=datetime.timedelta(minutes=10))
+def upload_to_gcs(filenames):
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(settings.CLOUD_BUCKET)
-
-    filename = "details.parq"
-    blob = bucket.blob(filename)
-    details.to_parquet(filename)
-    blob.upload_from_filename(filename)
-
-    filename = "leaderboard.parq"
-    blob = bucket.blob(filename)
-    leaderboard.to_parquet(filename)
-    blob.upload_from_filename(filename)
+    for filename in filenames:
+        blob = bucket.blob(filename)
+        blob.upload_from_filename(filename)
 
 
 @click.command()
@@ -41,7 +44,8 @@ def main(register, run):
         schedule = None
 
     with Flow("numerai-reports", schedule) as flow:
-        upload_to_gcs()
+        filenames = fetch()
+        upload_to_gcs(filenames)
 
     flow.storage = Docker(
         registry_url="gcr.io/numerai-171710",
@@ -49,12 +53,11 @@ def main(register, run):
         files={os.path.abspath("data.py"): "modules/data.py",
                os.path.abspath("settings.py"): "modules/settings.py",
                os.path.abspath("utils.py"): "modules/utils.py",
-
                },
         env_vars={"PYTHONPATH": "$PYTHONPATH:modules/"},)
 
     if register:
-        flow_id = flow.register(project_name="numerai", labels=["docker"])
+        flow.register(project_name="numerai", labels=["docker"])
     if run:
         flow.run()
 
